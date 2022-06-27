e4829f64c8374da7f491fa470f1630a6bf6f5084
Doğuş Akkaş <dogusakkas@gmail.com>
2022-06-27 16:57:37 +0300

    Inter-client messaging is provided

--- SignalRChatServerExample/Hubs/ChatHub.cs
+++ SignalRChatServerExample/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SignalRChatServerExample.Data;
 using SignalRChatServerExample.Models;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SignalRChatServerExample.Hubs
@@ -17,8 +18,32 @@ namespace SignalRChatServerExample.Hubs
 
             ClientSource.Clients.Add(client);
             await Clients.Others.SendAsync("clientJoined", nickName);
-
             await Clients.All.SendAsync("clients", ClientSource.Clients);
+        }
+
+        public async Task SendMessageAsync(string message, string clientName)
+        {
+            clientName = clientName.Trim();
+            Client senderClient = ClientSource.Clients.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (clientName == "Tümü")
+            {
+                await Clients.Others.SendAsync("receiveMessage", message, senderClient.NickName);
+            }
+            else
+            {
+                Client client = ClientSource.Clients.FirstOrDefault(x => x.NickName == clientName);
+                await Clients.Client(client.ConnectionId).SendAsync("receiveMessage", message, senderClient.NickName);
+            }
+
+        }
+
+        public async Task AddGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            GroupSource.Groups.Add(new Group { GroupName = groupName });
+
+            await Clients.All.SendAsync("groups", GroupSource.Groups);
+
         }
     }
 }
